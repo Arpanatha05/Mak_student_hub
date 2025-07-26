54acd9780e0195a02790ed24812cdea131035e5e
Arpanatha05 <arpanatha7@gmail.com>
2025-07-27 00:55:28 +0530

    dark and light

--- script.js
+++ script.js
@@ -21,3 +21,26 @@
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 }
+//dark and light mode
+const toggle = document.getElementById("theme-toggle");
+
+    // Apply saved theme
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.body.classList.add("dark-mode");
+      toggle.checked = true;
+    } else {
+      document.body.classList.add("light-mode");
+      toggle.checked = false;
+    }
+
+    // Toggle logic
+    toggle.addEventListener("change", () => {
+      if (toggle.checked) {
+        document.body.classList.replace("light-mode", "dark-mode");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.body.classList.replace("dark-mode", "light-mode");
+        localStorage.setItem("theme", "light");
+      }
+    });
